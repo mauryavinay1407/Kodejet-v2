eae7ccfb6f732a4f7ab5fc9c147bc9d37e028d09
mauryavinay1407 <vinaymaurya1407@gmail.com>
2025-03-11 15:22:06 +0530

    fix: update config

--- client/config.ts
+++ client/config.ts
@@ -1,4 +1,4 @@
 // config.js
 export const BASE_URL = import.meta.env.DEV
   ? "http://localhost:3000"  // Local backend
-  : "https://kodejet-backend.onrender.com"; // Deployed backend
+  : "https://kodejet-v2-backend.onrender.com"; // Deployed backend
